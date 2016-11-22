18701f61a5472070c3c687d2cd6092ff05a83d49
Matthias Otto <matthias.otto@devfacto.com>
2016-11-22 14:49:19 -0700

    Convert to Swift 3.0

--- KeychainTouchIDTest/AppDelegate.swift
+++ KeychainTouchIDTest/AppDelegate.swift
@@ -15,40 +15,39 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func promptTouchID() {
-        var myContext = LAContext()
+        let myContext = LAContext()
         var authError: NSError? = nil
-        var myLocalizedReasonString:String = "Authenticate using your finger"
-        if (myContext.canEvaluatePolicy(.DeviceOwnerAuthenticationWithBiometrics, error:&authError)) {
+        let myLocalizedReasonString:String = "Authenticate using your finger"
+        if (myContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error:&authError)) {
             
-            myContext.evaluatePolicy(.DeviceOwnerAuthenticationWithBiometrics,
+            myContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics,
                 localizedReason: myLocalizedReasonString,
-                reply: {(success: Bool, error: NSError!)->() in
+                reply: { (success: Bool, error: Error?) -> Void in
                     
                     if (success) {
-                        println("User authenticated")
-                    } else {
-                        switch (error.code) {
-                        case LAError.AuthenticationFailed.rawValue:
-                            println("Authentication Failed")
+                        print("User authenticated")
+                    } else if let error = error {
+                        switch (error) {
+                        case LAError.authenticationFailed:
+                            print("Authentication Failed")
                             
-                        case LAError.UserCancel.rawValue:
-                            println("User pressed Cancel button")
+                        case LAError.userCancel:
+                            print("User pressed Cancel button")
                             
-                        case LAError.UserFallback.rawValue:
-                            println("User pressed \"Enter Password\"")
+                        case LAError.userFallback:
+                            print("User pressed \"Enter Password\"")
                             
                         default:
-                            
-                            println("Touch ID is not configured")
+                            print("Touch ID is not configured")
                         }
                         
-                        println("Authentication Fails");
+                        print("Authentication Fails");
                         let alert = UIAlertView(title: "Error", message: "Auth fails!", delegate: nil, cancelButtonTitle:"OK")
                         alert.show()
                     }
             })
         } else {
-            println("Can not evaluate Touch ID");
+            print("Can not evaluate Touch ID");
             let alert = UIAlertView(title: "Error", message: "Your passcode should be set!", delegate: nil, cancelButtonTitle:"OK")
             alert.show()
         }
@@ -56,32 +55,32 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
     }
     
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         promptTouchID()
         return true
     }
     
-    func applicationWillResignActive(application: UIApplication) {
+    func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
     
-    func applicationDidEnterBackground(application: UIApplication) {
+    func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
     
-    func applicationWillEnterForeground(application: UIApplication) {
+    func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
         promptTouchID()
     }
     
-    func applicationDidBecomeActive(application: UIApplication) {
+    func applicationDidBecomeActive(_ application: UIApplication) {
         
         
     }
     
-    func applicationWillTerminate(application: UIApplication) {
+    func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
     
